002ae91e39084973612d235f3ebb74217682cbd6
martin <goudingcheng@gmail.com>
2025-06-24 22:00:51 +0800

    feature:sql row

---
+++ src/main/java/com/github/paohaijiao/model/JRow.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+package com.github.paohaijiao.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * packageName com.github.paohaijiao.model
+ *
+ * @author Martin
+ * @version 1.0.0
+ * @className JRow
+ * @date 2025/6/24
+ * @description
+ */
+public class JRow {
+
+    private Map<String, Object> rowData;
+    public JRow() {
+        this.rowData = new HashMap<>();
+    }
+
+    public void set(String columnName, Object value) {
+        rowData.put(columnName, value);
+    }
+
+    public Object get(String columnName) {
+        return rowData.get(columnName);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(String columnName, Class<T> type) {
+        Object value = rowData.get(columnName);
+        return value != null ? (T) value : null;
+    }
+
+    public boolean containsColumn(String columnName) {
+        return rowData.containsKey(columnName);
+    }
+
+    public Set<String> getColumnNames() {
+        return rowData.keySet();
+    }
+
+    public Object remove(String columnName) {
+        return rowData.remove(columnName);
+    }
+
+    public int size() {
+        return rowData.size();
+    }
+
+    public void clear() {
+        rowData.clear();
+    }
+
+    @Override
+    public String toString() {
+        return rowData.toString();
+    }
+}
